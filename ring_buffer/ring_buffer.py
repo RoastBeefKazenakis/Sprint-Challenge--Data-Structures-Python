a5daee8ba0f672dcef887cf9a0385d81ebfcb8d0
Thomas Sabino-Benowitz <sabinowitz@gmail.com>
2020-09-11 14:13:18 -0400

    MVP

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,16 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.contents = []
+        self.capacity = capacity
+        self.oldest_ext_ind = 0
 
     def append(self, item):
-        pass
+        if len(self.contents) < self.capacity:
+            self.contents.append(item)
+
+        elif len(self.contents) == self.capacity:
+            self.contents[self.oldest_ext_ind] = item
+            self.oldest_ext_ind = (self.oldest_ext_ind + 1) % self.capacity
 
     def get(self):
-        pass
+        return self.contents
